82bd3472d250af466edb0b25522dc651a963b4cd
Christian Valdez <christianvaldez36@hotmail.com>
2020-09-30 13:43:37 -0700

    modified test of app.js component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from Docker Compose</p>
+        <p>Hello from Docker</p>
       </header>
     </div>
   );
